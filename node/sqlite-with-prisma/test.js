f307b0b6ec51aae9452b0052cd4f7cb0da984c86
skypesky <2565978507@qq.com>
2022-10-16 14:34:39 +0800

    feat: improve sqlite

--- node/sqlite-with-prisma/test.js
+++ node/sqlite-with-prisma/test.js
@@ -2,7 +2,33 @@ const { PrismaClient } = require("@prisma/client");
 const prismaClient = new PrismaClient();
 
 async function main() {
-  // CRUD
+  // 删除记录
+  const batchPayload = await prismaClient.user.deleteMany();
+  console.log("delete", batchPayload.count);
 
-  const user = await prismaClient;
+  // 创建记录
+  const user = await prismaClient.user.create({
+    data: {
+      email: "2565978507@qq.com",
+      name: "skypesky",
+    },
+  });
+  console.log("create", user);
+
+  // 更新记录
+  const updatedUser = await prismaClient.user.update({
+    where: {
+      email: "2565978507@qq.com",
+    },
+    data: {
+      name: "skypesky666",
+    },
+  });
+  console.log("update", updatedUser);
+
+  // 查询记录
+  const users = await prismaClient.user.findMany();
+  console.log("find", users);
 }
+
+main();
