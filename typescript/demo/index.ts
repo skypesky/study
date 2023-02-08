c702297c4a6d4fdc4165ee78cba74dc9c0a00bb7
skypesky <2565978507@qq.com>
2023-02-09 00:46:43 +0800

    feat: add typescript demo

---
+++ typescript/demo/index.ts
@@ -0,0 +1,5 @@
+console.log("hello1");
+// This is an industrial-grade general-purpose greeter function:
+const greet = (person, date) => {
+  console.log(`Hello ${person}, today is ${[...date]}!`);
+};
