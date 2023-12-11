1c90df7de5ebd417977878d3e3f1e984dc6d006c
skypesky <2565978507@qq.com>
2023-12-12 00:32:32 +0800

    feat: add day003,4

---
+++ python/day100/day03.py
@@ -0,0 +1,7 @@
+username = input('请输入用户名: ')
+password = input('请输入口令: ')
+# 用户名是admin且密码是123456则身份验证成功否则身份验证失败
+if username == 'admin' and password == '123456':
+    print('身份验证成功!')
+else:
+    print('身份验证失败!')
---
+++ python/day100/day04.py
@@ -0,0 +1,8 @@
+
+
+sum = 0
+
+for x in range(3):
+    sum += x
+
+print('sum is', sum)
