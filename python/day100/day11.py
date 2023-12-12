4d798a9e7a8ab8fe20b0103ef565857fe8af07db
skypesky <2565978507@qq.com>
2023-12-12 23:49:34 +0800

    feat: add day11

---
+++ python/day100/day11.py
@@ -0,0 +1,14 @@
+def main():
+    try:
+        with open('致橡树.txt', 'r', encoding='utf-8') as f:
+            print(f.read())
+    except FileNotFoundError:
+        print('无法打开指定的文件!')
+    except LookupError:
+        print('指定了未知的编码!')
+    except UnicodeDecodeError:
+        print('读取文件时解码错误!')
+
+
+if __name__ == '__main__':
+    main()
