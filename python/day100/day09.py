d6c988e57c4708e4bd27bf1e95fed2a81c0152a6
skypesky <2565978507@qq.com>
2023-12-12 23:17:45 +0800

    feat: add day09, day10

---
+++ python/day100/day09.py
@@ -0,0 +1,38 @@
+class Person(object):
+
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+
+    # 访问器 - getter方法
+    @property
+    def name(self):
+        return self._name
+
+    # 访问器 - getter方法
+    @property
+    def age(self):
+        return self._age
+
+    # 修改器 - setter方法
+    @age.setter
+    def age(self, age):
+        self._age = age
+
+    def play(self):
+        if self._age <= 16:
+            print('%s正在玩飞行棋.' % self._name)
+        else:
+            print('%s正在玩斗地主.' % self._name)
+
+
+def main():
+    person = Person('王大锤', 12)
+    person.play()
+    person.age = 22
+    person.play()
+    # person.name = '白元芳'  # AttributeError: can't set attribute
+
+
+if __name__ == '__main__':
+    main()
---
+++ python/day100/day10.py
@@ -0,0 +1,29 @@
+import pygame
+
+
+def main():
+    # 初始化导入的pygame中的模块
+    pygame.init()
+    # 初始化用于显示的窗口并设置窗口尺寸
+    screen = pygame.display.set_mode((800, 600))
+    # 设置当前窗口的标题
+    pygame.display.set_caption('大球吃小球')
+    # 定义变量来表示小球在屏幕上的位置
+    x, y = 50, 50
+    running = True
+    # 开启一个事件循环处理发生的事件
+    while running:
+        # 从消息队列中获取事件并对事件进行处理
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((255, 255, 255))
+        pygame.draw.circle(screen, (255, 0, 0,), (x, y), 30, 0)
+        pygame.display.flip()
+        # 每隔50毫秒就改变小球的位置再刷新窗口
+        pygame.time.delay(50)
+        x, y = x + 5, y + 5
+
+
+if __name__ == '__main__':
+    main()
