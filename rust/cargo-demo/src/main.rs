c46b50b763dcaf35828868979d9245f978724ad8
JianChao <guhuo13@gmail.com>
2026-03-23 15:40:52 +0800

    feat: add guess attempt counter in cargo-demo

--- rust/cargo-demo/src/main.rs
+++ rust/cargo-demo/src/main.rs
@@ -8,19 +8,22 @@ fn main() {
     // println!("The secret number is: {secret_number}");
     println!("Please input your guess.");
 
+    let mut attempts = 0;
     loop {
+        attempts += 1;
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-        let guess: u32 = guess.parse().expect("Please type a number!\n");
-        println!("You guessed: {guess}");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!\n");
+        println!("You guessed use {attempts} times: {guess}");
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
                 println!("You win!");
+                println!("It took you {attempts} guesses.");
                 break;
             }
         }
